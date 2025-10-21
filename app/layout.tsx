3cff200da4b00ced830bfb648bb0f210d550021f
Saiful Islam <saifulislamwebdev@gmail.com>
2025-10-21 12:58:40 +0600

    input Changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import "./style.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "SmartGen Digital| Full Stack Digital Agency",
@@ -34,12 +23,12 @@ export default function RootLayout({
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-brands/css/uicons-brands.css'></link>
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-straight/css/uicons-regular-straight.css'></link>
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Header/>
         <div className="messenger">
           <a href="https://www.facebook.com/smartgendigitalagency"
           target="_blank"><i className="fi fi-brands-facebook-messenger"
-            rel="noopener noreferrer"
+          rel="noopener noreferrer"
           ></i></a>
         </div>
         {children}
